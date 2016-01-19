f5ed0bac7727676d92b02ab146b4b96498c7271c
RadhaRamachandran <radha.ramachdran@gmail.com>
2016-01-19 17:14:39 -0600

    Fixed errors with start & end indices + calculations

--- WattTime API/WattTime-Display.py
+++ WattTime API/WattTime-Display.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[27]:
 
 from bibliopixel import *
 from bibliopixel.drivers.LPD8806 import *
@@ -11,7 +11,7 @@ import csv
 import numpy as np
 
 
-# In[2]:
+# In[28]:
 
 LedsPerSide = 8
 
@@ -25,7 +25,7 @@ led=LEDStrip(driver)
 
 # ### Import data
 
-# In[3]:
+# In[29]:
 
 clean_ratio = [] 
 with open('Carbon_lbsperMW_monthly_average.csv') as csvfile:
@@ -63,7 +63,7 @@ while(green > 0): ## start with red + green and decrease green
     color_gn_rd.append((green,red,0)); 
 
 
-# In[6]:
+# In[25]:
 
 ### from US Energy Information Agency Website: https://www.eia.gov/tools/faqs/faq.cfm?id=74&t=11
 ### Max value - 2170 for Lignite
@@ -77,10 +77,14 @@ while(green > 0): ## start with red + green and decrease green
 color_disp = []
 ledON = []
 for a,b,c in clean_ratio: 
-    color_disp.append(np.floor(int(b)/100))
+    #print a,b,c
+    #print np.floor(np.float(c)/100)
+    #print np.floor(np.remainder(np.float(c),100))
+    #print np.floor(np.remainder(np.float(c),100) * LedsPerSide/100)
+    color_disp.append(np.floor(np.float(c)/100))
     ## 100's position represented by color bar --> going from green to red for 100 to 2000 ish
     
-    ledON.append(np.floor(np.remainder(int(b),100) * LedsPerSide/100))
+    ledON.append(np.floor(np.remainder(np.float(c),100) * LedsPerSide/100))
     
     ## 0 - 100 scaled by the number of lamps on each side
     ## x is the user-defined number of Led's per side
@@ -92,10 +96,10 @@ for a,b,c in clean_ratio:
 
 DisplaySide = 1 # Set the side on which the data will be displayed here.
 FirstLed_bottom = LedsPerSide*(DisplaySide-1)
-FinalLed_bottom = LedsPerSide*DisplaySide
+FinalLed_bottom = LedsPerSide*DisplaySide - 1
 
 FirstLed_top = LedsPerSide*(DisplaySide + 4 -1)
-FinalLed_top = LedsPerSide*(DisplaySide + 4 -1)
+FinalLed_top = LedsPerSide*(DisplaySide + 4 ) - 1
 
 
 # In[8]:
@@ -115,9 +119,9 @@ year = 2014
 months = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 
-# In[ ]:
+# In[26]:
 
-led.fill((0,0,0) # All LEDs off.
+#led.fill((0,0,0) # All LEDs off.
          
 while True: 
     for i, month in enumerate(months):
@@ -125,17 +129,22 @@ while True:
 
         #Set bottom panel
         led.fill((0,0,0),start=FirstLed_bottom, end=FinalLed_bottom)
+        #print ((0,0,0),FirstLed_bottom, FinalLed_bottom)
         led.update()
         led_set(FirstLed_bottom, np.int(ledON[i]), color_gn_rd[np.int(color_disp[i])]) 
+        print(FirstLed_bottom, np.int(ledON[i]), color_gn_rd[np.int(color_disp[i])]) 
+
         #ledON gives the number of LED's that will be turned on
         #color_disp specifies color
         
         #Turn all lights off top    
         led.fill((0,0,0),start=FirstLed_top, end=FinalLed_top)
+        #print((0,0,0),FirstLed_top, FinalLed_top)
         led.update()
         #Set top panel
         led_set(FirstLed_top + 2*np.int(np.floor(i/3)), 2, (200,150,100))
-       
+        #print (FirstLed_top + 2*np.int(np.floor(i/3)), 2, (200,150,100))
+
 
         time.sleep(2.)
         
